469f6edb9f42412a7d95f7353c5d36c1a4cf138f
Zhan Hodzhabergenov <zhan.ernesuly2@gmail.com>
2025-03-23 14:40:44 +0500

    lab11 update

---
+++ week6/final_project/rtl/bitty_core.v
@@ -0,0 +1,25 @@
+module bitty_core (
+    input   wire        clk,
+    input   wire        reset,
+    input   wire        run,
+    input   wire [15:0] instruction,
+    output  wire        done
+);
+
+    wire sel;
+    wire mode;
+    wire en_s;
+    wire en_c;
+    wire en_0;
+    wire en_1;
+    wire en_2;
+    wire en_3;
+    wire en_4;
+    wire en_5;
+    wire en_6;
+    wire en_7;
+    wire en_i;
+
+    
+
+endmodule
